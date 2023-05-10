b1e2176a230b58396daf34935a778c781b56e210
jskahl <josekahl333@gmail.com>
2023-05-10 16:05:05 -0300

    Adição - Adição do model Modelo; - Adição do model Acessorio; - Adição do model Cor; - Adição do model Veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Marca, Categoria
+from garagem.models import Marca, Modelo, Acessorio, Cor, Veiculo
 
 admin.site.register(Marca)
-admin.site.register(Categoria)
+admin.site.register(Modelo)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -1,14 +1,41 @@
 from django.db import models
 
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome.upper()
-    
-class Categoria(models.Model):
+
+
+class Modelo(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(null=True, blank=True, default=1880)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} {self.ano} {self.cor}"
