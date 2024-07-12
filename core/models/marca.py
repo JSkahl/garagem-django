7b10ce86804a636a79ca6cecca96e07d2231cd30
JSkahl <josekahl333@gmail.com>
2024-07-12 09:31:53 -0300

    chore(model): Adição do id no retorno das strings no Admin

--- core/models/marca.py
+++ core/models/marca.py
@@ -5,4 +5,4 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return f"{self.nome.upper()} ({self.nacionalidade})"
+        return f"{self.nome.upper()} ({self.nacionalidade}) [{self.id}]"
