512d9d728b8af48af130b450ec59709a572f2f56
jskahl <josekahl333@gmail.com>
2024-07-16 09:18:14 -0300

    fix(model): Ajustando a model Marca para não aparecer a nacionalidade caso não haja uma

--- core/models/marca.py
+++ core/models/marca.py
@@ -5,4 +5,8 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return f"{self.nome.upper()} ({self.nacionalidade}) [{self.id}]"
+        if self.nacionalidade:
+            return f"{self.nome.upper()} ({self.nacionalidade}) [{self.id}]"
+        else:
+            return f"{self.nome.upper()} [{self.id}]"
+        
